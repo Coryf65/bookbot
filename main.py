424ab0c871339b01153851f3c8eb0837764cfd10
Coryf65 <Coryf65@users.noreply.github.com>
2024-11-10 21:31:58 -0600

    new word counter added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 
 
 # read file and print text
-def ReadBookFile(path):
+def read_book_from_file(path):
     file_contents = ""
 
     with open(path) as f:
@@ -14,11 +14,18 @@ def ReadBookFile(path):
     return file_contents
 
 
+def count_words(text):
+   split = text.split()
+   return len(split)
+   
+
 # Gather our code in a main() function
 def main():
    book_path = "books/frankenstien.txt"
-   book_data = ReadBookFile(book_path)
-   print(book_data)
+   book_data = read_book_from_file(book_path)
+   #print(book_data)
+   word_count = count_words(book_data)
+   print(f"{word_count} words found in the document")
 
 if __name__ == '__main__':
   main()
