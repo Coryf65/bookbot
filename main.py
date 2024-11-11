66cece1f8150ca6cf6239a9186c173004ccf28c3
Coryf65 <Coryf65@users.noreply.github.com>
2024-11-10 22:28:19 -0600

    working on the character counter.

--- main.py
+++ main.py
@@ -6,25 +6,23 @@ import sys
 
 # read file and print text
 def read_book_from_file(path):
-    '''
-    Reads the contents of a text file (book text) and returns it.
+  '''
+		Reads the contents of a text file (book text) and returns it.
 
     Args:
-        path (string): The path to the file to read.
+			path (string): The path to the file to read.
 
-    Returns:
-        string: all of the text in the given file.
-   '''
-    file_contents = ""
-
-    with open(path) as f:
-        file_contents = f.read()
-
-    return file_contents
-
+		Returns:
+			string: all of the text in the given file.
+  '''
+  file_contents = ""
+  with open(path) as f:
+    file_contents = f.read()
+  
+  return file_contents
 
 def count_words(text):
-   '''
+  '''
     Counts the total number of words and returns the counter.
     uses a space as the delimeter.
 
@@ -34,9 +32,29 @@ def count_words(text):
     Returns:
         int: The total number of words counted.
    '''
-   split = text.split()
-   return len(split)
-   
+  split = text.split()
+  return len(split)
+
+def count_characters(text):
+    '''
+     Counts the total number of characters used and 
+     returns a dictionary with each letter and how many times it occured.
+
+    Args:
+        text (string): The text to parse.
+
+    Returns:
+        {'p': 6121, 'r': 20818 }: returns a dictionary with each letter and how many times it occured.
+    '''
+    all_characters = {}
+
+    # need to lood through every character and count
+    for character in text:
+      print(character)
+      all_characters[character] = all_characters[character]   
+      print(all_characters[character]) 
+    return text
+
 
 # Gather our code in a main() function
 def main():
@@ -45,6 +63,8 @@ def main():
    #print(book_data)
    word_count = count_words(book_data)
    print(f"{word_count} words found in the document")
+   
+   count_characters("cory test one two")
 
 if __name__ == '__main__':
   main()
