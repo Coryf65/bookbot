2bb3d61dc5c7e8736700ceab13fc83ddafa72333
Coryf65 <Coryf65@users.noreply.github.com>
2024-11-10 20:37:56 -0600

    reading a book and printing to cli

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+#!/usr/bin/python3
+
+# import modules used here -- sys is a very standard one
+import sys
+
+
+# read file and print text
+def ReadBookFile(path):
+    file_contents = ""
+
+    with open(path) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+# Gather our code in a main() function
+def main():
+   book_path = "books/frankenstien.txt"
+   book_data = ReadBookFile(book_path)
+   print(book_data)
+
+if __name__ == '__main__':
+  main()
