ae9467398a95a763c26b85c5f80c14bb9b3ebd7c
Coryf65 <Coryf65@users.noreply.github.com>
2024-11-11 16:05:09 -0600

    some cleanup and removing spaces

--- main.py
+++ main.py
@@ -49,12 +49,12 @@ def count_characters(text):
     all_characters = {}
     # need to lood through every character and count
     for character in text.lower():
-      #print(f"char: '{character}' is in the dict '{character in all_characters}")      
+      #print(f"char: '{character}' is in the dict '{character in all_characters}")
       if character in all_characters:
         # up by one
-        all_characters[character] = all_characters[character] + 1
-      else:        
-        all_characters[character] = 1      
+        all_characters[character] += 1
+      else:
+        all_characters[character] = 1
     return all_characters
 
 
@@ -64,7 +64,7 @@ def main():
    book_data = read_book_from_file(book_path)
    print(book_data)
    word_count = count_words(book_data)
-   print(f"{word_count} words found in the document")   
+   print(f"{word_count} words found in the document")
    characters = count_characters(book_data)
    print(f"total characters used in this book: {characters}")
 
